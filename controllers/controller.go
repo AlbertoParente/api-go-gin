4b9f5e66be269bfd58481eb9be8222856cac2ae7
albertoparente <albertoparentefh@gmail.com>
2022-02-16 23:25:10 -0300

    Creating Student View

--- controllers/controller.go
+++ controllers/controller.go
@@ -1,12 +1,17 @@
 package controllers
 
 import (
+	"net/http"
+
+	"github.com/api-go-gin/database"
 	"github.com/api-go-gin/models"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowAllStudents(c *gin.Context) {
-	c.JSON(200, models.Students)
+	var students []models.Student
+	database.DB.Find(&students)
+	c.JSON(200, students)
 }
 
 func Greeting(c *gin.Context) {
@@ -14,4 +19,28 @@ func Greeting(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"API:": "It's all OK " + name + "...?",
 	})
+}
+
+func CreateNewStudent(c *gin.Context) {
+	var student models.Student
+	if err := c.ShouldBindJSON(&student); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	database.DB.Create(&student)
+	c.JSON(http.StatusOK, student)
+}
+
+func GetById(c *gin.Context) {
+	var student models.Student
+	id := c.Params.ByName("id")
+	database.DB.First(&student, id)
+	if student.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"Not Found": "Student not found...!"})
+		return
+	}
+
+	c.JSON(http.StatusOK, student)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -9,5 +9,10 @@ func HandleRequests() {
 	r := gin.Default()
 	r.GET("/students", controllers.ShowAllStudents)
 	r.GET("/:name", controllers.Greeting)
+	r.POST("/students", controllers.CreateNewStudent)
 	r.Run()
+}
+
+func getById(c *gin.Context) {
+
 }
